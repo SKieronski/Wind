522f24bf5f14e16274b8b0c9bcef2c7f8961cf6f
SKieronski <GitPW123!@#>
2022-03-11 15:05:20 -0600

    Using Directions API to draw a route between two points on the map
    .

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,23 @@
 import React, {useContext} from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyDD-zJvuFYllxvpWbNVZa5urbVW-y0WDx4'
 
 const Map = () => {
     const initialLocation = {
         longitude: -122.0312186,
         latitude: 37.33233141,
+    };
+    const destination = {
+        longitude: -122.0282186,
+        latitude: 37.32733141,
     }
+    
     return (
         <MapView
+            provider={PROVIDER_GOOGLE}
             style={styles.map}
             initialRegion={{
                 ...initialLocation,
@@ -16,7 +25,12 @@ const Map = () => {
                 longitudeDelta: 0.01
             }}
         >
-            
+            <MapViewDirections
+                origin={initialLocation}
+                destination={destination}
+                apikey={GOOGLE_MAPS_APIKEY}
+                mode="WALKING"
+            />
         </MapView>
     )
 }
